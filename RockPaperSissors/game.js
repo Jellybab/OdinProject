17d676bcd66dd099cd2c400f476f39ea2fc9d78d
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-07-12 14:19:49 +0100

    made each button into its own div

--- RockPaperSissors/game.js
+++ RockPaperSissors/game.js
@@ -1,7 +1,7 @@
 
-function playerChoice(buttonClicked){
+function playerChoice(buttonClicked) {
     let choice = buttonClicked.target.textContent.toLowerCase();
-    switch(choice){
+    switch (choice) {
         case choices[0]:
             return 0;
         case choices[1]:
@@ -13,37 +13,67 @@ function playerChoice(buttonClicked){
     }
 }
 
-function computerChoice(){
+function computerChoice() {
     let choice = Math.floor(Math.random() * choices.length);
     return choice;
 }
 
-function playGame(buttonClicked){
+function showResults(playerWin) {
+
+    const resultsSection = document.querySelector('#results');
+
+    let newRound = document.createElement('div');
+    newRound.setAttribute('class', 'round');
+    newRound.setAttribute('id', 'round_'+currentRound)
+
+    let roundTitle = document.createElement('h2');
+    roundTitle.setAttribute('class', 'round_title')
+    newRound.textContent = 'Round ' + currentRound; 
+    newRound.appendChild(roundTitle);
+    
+    let roundAnnounce = document.createElement('p');
+    if(playerWin){roundAnnounce.textContent = 'Well done you won this round';}
+    else{roundAnnounce.textContent = 'Too bad you lose';}
+    newRound.appendChild(roundAnnounce);
+
+    resultsSection.insertBefore(newRound, resultsSection.childNodes[0]);
+
+
+
+    currentRound++;
+}
+
+function playGame(buttonClicked) {
     let player = playerChoice(buttonClicked);
     let computer = computerChoice();
-
-    if(player == 3 ){
+    let playerWin=false;
+    if (player == 3) {
         console.log('dont try and cheat');
     }
-    else if (player == computer){
+    else if (player == computer) {
         console.log('its a draw');
     }
     else if (player == 0 && computer == 2 ||
         player == 1 && computer == 0 ||
-        player == 2 && computer == 1){
-            console.log('well done you won');
-            playerWins++;
-        }
-    else{
+        player == 2 && computer == 1) {
+        console.log('well done you won');
+        playerWins++;
+        playerWin = true;
+    }
+    else {
         console.log('you lose');
         computerWins++;
     }
-    
+
+    showResults(playerWin);
 }
+
+
 //game data
 const choices = ['rock', 'paper', 'scissors'];
 let playerWins = 0;
 let computerWins = 0;
+let currentRound = 1;
 const rounds = 5;
 
 //getting buttons 
