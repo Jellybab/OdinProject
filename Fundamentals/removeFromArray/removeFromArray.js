2b87b0ca9231b5ec3fa9e75c00b5bbb0df55446b
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-07-20 19:43:12 +0100

    changed to only same type

--- Fundamentals/removeFromArray/removeFromArray.js
+++ Fundamentals/removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
 const removeFromArray = function(arr, ...args) {
     for (let i = 0; i < args.length; i++){
         for (let j = 0; j < arr.length; j++){
-            if(arr[j] == args[i]){
+            if(arr[j] === args[i]){
                 arr.splice(j, 1);
             }
         }
