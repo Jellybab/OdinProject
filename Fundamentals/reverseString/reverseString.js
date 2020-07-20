cf43f8d68cc2fa4afbf24b1d27123b9b1d2aacfd
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-07-20 19:22:20 +0100

    counts from string length -1 instead of the length

--- Fundamentals/reverseString/reverseString.js
+++ Fundamentals/reverseString/reverseString.js
@@ -1,6 +1,6 @@
 const reverseString = function(string) {
     let reversedString = "";
-    for (let i = string.length; i >= 0; i--){
+    for (let i = string.length-1; i >= 0; i--){
         reversedString += string[i];
     }
     return reversedString;
