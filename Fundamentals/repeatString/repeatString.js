ce9fc0656b03db49238fdc933089640be01b8e61
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-07-20 19:07:56 +0100

    correct errors in varibles

--- Fundamentals/repeatString/repeatString.js
+++ Fundamentals/repeatString/repeatString.js
@@ -1,9 +1,9 @@
 const repeatString = function(string, amount) {
     let repeatingString = "";
     for( let i = 0; i = amount.length; i++){
-        repeatString+= string;
+        repeatingString += string;
     }
-    return string;
+    return repeatingString;
 }
 
 module.exports = repeatString
