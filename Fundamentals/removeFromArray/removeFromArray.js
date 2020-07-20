98d59f8932941b36142dd268e5a104e10078c0b0
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-07-20 19:34:28 +0100

    code added

--- Fundamentals/removeFromArray/removeFromArray.js
+++ Fundamentals/removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    let arr = args[0];
+    for (let i = 1; i < args.length; i++){
+        for (let j = 0; j < arr.length-1; j++){
+            if(arr[j] == args[1]){
+                arr.splice(j, 1);
+            }
+        }
+    }
+    return arr;
 }
 
 module.exports = removeFromArray
