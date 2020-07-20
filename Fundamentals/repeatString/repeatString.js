a2333dd19075b481af0e28d17a20d54eeb66ef78
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-07-20 19:02:47 +0100

    added code to repeating string

--- Fundamentals/repeatString/repeatString.js
+++ Fundamentals/repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, amount) {
+    let repeatingString = "";
+    for( let i = 0; i = amount.length; i++){
+        repeatString+= string;
+    }
+    return string;
 }
 
 module.exports = repeatString
