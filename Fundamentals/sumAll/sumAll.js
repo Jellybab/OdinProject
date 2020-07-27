615e7efe742dd6b5825811c48eb4fb5f90880d66
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-07-27 17:52:10 +0100

    did first version of code

--- Fundamentals/sumAll/sumAll.js
+++ Fundamentals/sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(fromNum, toNum) {
+    if ((typeof fromNum === "number") && (typeof toNum === "number")){ 
+        let lowest = 0;
+        let highest = 0;
+        let sum = 0;
+        (fromNum <= toNum) ? 
+            (lowest = fromNum, highest = toNum) :
+            (lowest = toNum, highest = fromNum);
 
+        for (lowest; lowest<= highest; lowest++){
+            sum += lowest;
+        }
+        return sum;
+    }
+    else{
+        return 'ERROR';
+    }
+    
+    
 }
 
 module.exports = sumAll
