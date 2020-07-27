cfdb5501e206332a132cfec2dfd30fda8b38fca0
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-07-27 20:25:38 +0100

    added basic code for tempConversion

--- Fundamentals/tempConversion/tempConversion.js
+++ Fundamentals/tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  return temp = ((temp-32)*5/9  ).toFixed(1);
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return temp = (9/5*32+temp).toFixed(1);
 }
 
 module.exports = {
