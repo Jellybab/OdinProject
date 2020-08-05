967d0b3775815e20f0365d7c2de765aa2670f003
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-08-06 00:26:58 +0100

    corrected another variable

--- Fundamentals/palindromes/palindromes.js
+++ Fundamentals/palindromes/palindromes.js
@@ -1,13 +1,13 @@
-const palindromes = function(string) {
+const palindromes = function(stringToPalindrome) {
     let stringToCheck = '';
     let stringTest = /\w/i;
-    for(let i = 0; i < string.length; i++){
+    for(let i = 0; i < stringToPalindrome.length; i++){
         if(stringTest.test(string[i])){
             stringToCheck += string[i];
         }
     }
     let palindromedString = '';
-    for (let i = (string.length-1); i >= 0; i--){
+    for (let i = (stringToCheck.length - 1); i >= 0; i--){
         palindromedString += stringToCheck[i];
     }
     console.log(palindromedString);
