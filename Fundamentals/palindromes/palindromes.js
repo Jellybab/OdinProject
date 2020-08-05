79247562136afc8d9723e0a4de866a22de314e52
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-08-06 00:15:34 +0100

    changed code to get rid of non-letter characters

--- Fundamentals/palindromes/palindromes.js
+++ Fundamentals/palindromes/palindromes.js
@@ -1,4 +1,10 @@
 const palindromes = function(string) {
+    let stringToCheck = '';
+    for(let i = 0; i < string.length; i++){
+        if("/\w/".test(string[i])){
+            stringToCheck += string[i];
+        }
+    }
     let palindromedString = '';
     for (let i = (string.length-1); i >= 0; i--){
         palindromedString += string[i];
