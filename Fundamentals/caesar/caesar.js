ab955213143d25a5abba34f62cf889da56ec4cbc
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-08-06 00:49:00 +0100

    added basic code

--- Fundamentals/caesar/caesar.js
+++ Fundamentals/caesar/caesar.js
@@ -1,5 +1,22 @@
-const caesar = function() {
-
+const caesar = function(string, key) {
+    let caesarString = '';
+    let letters = /\w/i;
+    for(let i = 0; i < string.length; i++){
+        let letterToAdd = string[i];
+        if(letters.test(letterToAdd)){
+            if(/[A-Z]/.test(letterToAdd)){
+                letterToAdd = String.fromCharCode(
+                    (letterToAdd.charCodeAt(i) + key - 65)
+                     % 26 + 65);
+            }
+            else if(/[a-z]/.test(letterToAdd)){
+                letterToAdd = String.fromCharCode(
+                    (letterToAdd.charCodeAt(i) + key - 97)
+                     % 26 + 97);
+            }
+        }
+        caesarString += letterToAdd;
+    }
 }
 
 module.exports = caesar
