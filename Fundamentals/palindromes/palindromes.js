3f2f8926b104eb2b30b57a1183e148dcf1fd3c27
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-08-06 00:03:17 +0100

    added console.log

--- Fundamentals/palindromes/palindromes.js
+++ Fundamentals/palindromes/palindromes.js
@@ -3,6 +3,7 @@ const palindromes = function(string) {
     for (let i = (string.length-1); i >= 0; i--){
         palindromedString += string[i];
     }
+    console.log(palindromedString);
     if (palindromedString === string){
         return true;
     }
