58c97f38c496ad55136b2ccbd9dc40afbee89a21
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-08-06 00:01:30 +0100

    palindromes basic code

--- Fundamentals/palindromes/palindromes.js
+++ Fundamentals/palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    let palindromedString = '';
+    for (let i = (string.length-1); i >= 0; i--){
+        palindromedString += string[i];
+    }
+    if (palindromedString === string){
+        return true;
+    }
+    return false;
 }
 
 module.exports = palindromes
