e9d6df3687c10f3806413dd0ce05080f96522e5e
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-08-06 00:16:32 +0100

    put test string into its own let

--- Fundamentals/palindromes/palindromes.js
+++ Fundamentals/palindromes/palindromes.js
@@ -1,7 +1,8 @@
 const palindromes = function(string) {
     let stringToCheck = '';
+    let stringTest = "/\w/";
     for(let i = 0; i < string.length; i++){
-        if("/\w/".test(string[i])){
+        if(stringTest.test(string[i])){
             stringToCheck += string[i];
         }
     }
