de79fe3cfef5f2003dfdc3231bad734b67dc005a
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-08-06 22:21:58 +0100

    changed code alot

--- Fundamentals/findTheOldest/findTheOldest.js
+++ Fundamentals/findTheOldest/findTheOldest.js
@@ -1,19 +1,14 @@
 let findTheOldest = function(people) {
-    let oldest = people[0];
+    let oldest;
+    let oldestAge = 0;
     people.forEach(person => {
-        if (typeof person.yearOfDeath != 'undefined'){
-            oldest = ((oldest.yearOfDeath - oldest.yearOfBirth) >= 
-                (person.yearOfDeath - person.yearOfBirth)) ?
-                    oldest : person;
-        }
-        else{
-            let thisYear = new Date().getFullYear();
-            oldest = ((oldest.yearOfDeath - oldest.yearOfBirth) >= 
-                (thisYear - person.yearOfBirth)) ?
-                    oldest : person;
-            console.log(thisYear);
-            console.log(person.yearOfBirth);
-            console.log(thisYear - person.yearOfBirth);
+        let deathYear = (typeof person.yearOfDeath != 'undefined') ?
+         person.yearOfDeath : new Date().getFullYear();
+
+        let personAge = deathYear - person.yearOfBirth;
+        if(oldestAge < personAge) {
+            oldestAge = personAge;
+            oldest = person;
         }
     });
     return oldest;
