80a2ae59292d19d3776287f594f1e0883aa86246
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-08-06 18:31:25 +0100

    added 0 to check

--- Fundamentals/fibonacci/fibonacci.js
+++ Fundamentals/fibonacci/fibonacci.js
@@ -2,7 +2,8 @@ const fibonacci = function(num) {
     if(typeof num === 'string'){
         num = parseInt(num)
     }
-    if(num <= 0) return "OOPS"
+    if(num < 0) return "OOPS"
+    if(num == 0) return 0;
     else if(num < 2){
         return 1;
     }
