fd59d0f710f3ea83e2b881697bf7599a84bf2e0b
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-08-06 18:28:29 +0100

    change formula abit

--- Fundamentals/fibonacci/fibonacci.js
+++ Fundamentals/fibonacci/fibonacci.js
@@ -2,7 +2,7 @@ const fibonacci = function(num) {
     if(typeof num === 'string'){
         num = parseInt(num)
     }
-    if(num < 0) return "OOPS"
+    if(num <= 0) return "OOPS"
     else if(num < 2){
         return 1;
     }
