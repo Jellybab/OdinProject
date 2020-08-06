371b16adbca7ef938fcfe9daab3c868a3d709d8a
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-08-06 21:52:50 +0100

    find the oldest done

--- Fundamentals/findTheOldest/findTheOldest.js
+++ Fundamentals/findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    let oldest = people[0];
+    people.forEach(person => {
+        if (typeof person.yearOfDeath != 'undefined'){
+            oldest = ((oldest.yearOfDeath - oldest.yearOfBirth) < 
+                (person.yearOfDeath - person.yearOfBirth)) ?
+                    person : oldest;
+        }
+        else{
+            let date = new Date().getFullYear;
+            oldest = ((oldest.yearOfDeath - oldest.yearOfBirth) < 
+                (date - person.yearOfBirth)) ?
+                    person : oldest;
+        }
+    });
+    return oldest;
 
 }
 
