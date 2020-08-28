08047864f133b7925df57895aa178fd3570aaa77
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-08-28 17:43:19 +0100

    found error with a converting to L

--- ruby/caesar.rb
+++ ruby/caesar.rb
@@ -4,14 +4,13 @@ def caesar(message, factor)
     factor = factor % 26
     puts factor
     message.bytes.map do |byte|
-        if(byte >= 65 && byte <= 97)
+        if(byte >= 65 && byte <= 90)
             ((byte + factor - 65) % 26 + 65).chr
         elsif(byte >= 97 && byte <= 122)
             ((byte + factor - 97) % 26 + 97).chr
         else
             byte.chr
         end
-    binding.pry
     end.join
 end
 
