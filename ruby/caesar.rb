e92348a05f4a6f66a5511eab3628daa1208294f6
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-08-28 17:30:05 +0100

    first test at caesar

--- ruby/caesar.rb
+++ ruby/caesar.rb
@@ -1,9 +1,18 @@
 
 
-def caesar(string, factor)
+def caesar(message, factor)
     factor = factor % 26
     puts factor
+    message.bytes.map do |byte|
+        if(byte >= 65 && byte <= 97)
+            ((btye + factor - 65) % 26 + 65).chr
+        elsif(byte >= 97 && byte <= 122)
+            ((btye + factor - 97) % 26 + 97).chr
+        else
+            byte.chr
+        end
+    end.join
 end
 
 
-caesar("hello", 40);
+p cipher("What a string!", 5)
