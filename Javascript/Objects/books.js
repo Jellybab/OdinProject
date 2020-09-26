6451e7459fa00719a2bb45623c2f1231590cf337
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-09-26 17:58:26 +0100

    function for making new cards from book objects

--- Javascript/Objects/books.js
+++ Javascript/Objects/books.js
@@ -13,12 +13,48 @@ function Book(title, author, pages){
     }
 }
 
-let myLibrary = [];
+function addNewCard(book){
+    const cards = document.querySelector('#book_cards');
+
+    let newCard = document.createElement('article');
+    newCard.setAttribute('class', 'card');
+
+    let title = document.createElement('div');
+    title.setAttribute('class', 'title');
+    title.textContent = book.title;
+    newCard.appendChild(title);
+
+    let author = document.createElement('div');
+    author.setAttribute('class', 'author');
+    author.textContent = book.author;
+    newCard.appendChild(author);
+
+    let pages = document.createElement('div');
+    pages.setAttribute('class', 'pages');
+    pages.textContent = book.pages + ' pages';
+    newCard.appendChild(pages);
+
+    let read = document.createElement('div');
+    read.setAttribute('class', 'read');
+    read.textContent = (book.read === true) ? 'has read' : 'not read yet';
+    newCard.appendChild(read);
+
+    cards.appendChild(newCard);
+
+}
 
 function addBookToLibrary(book){
     myLibrary.push(book);
+    addNewCard(book);
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295)
-addBookToLibrary(theHobbit);
-console.log(myLibrary[0].info());
+function makeCards(library){
+    library.forEach(book => {
+        addNewCard(book);
+    });
+}
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295);
+const testBook = new Book('test', 'test', 1);
+
+let myLibrary = [theHobbit, testBook];
+makeCards(myLibrary);
