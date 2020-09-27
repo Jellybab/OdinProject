0de5ff5a72919caebe5f20bcad98d20c5571e061
Jellybab <38954680+Jellybab@users.noreply.github.com>
2020-09-27 17:37:09 +0100

    added form to html

--- Javascript/Objects/books.js
+++ Javascript/Objects/books.js
@@ -1,5 +1,6 @@
 let myLibrary = [];
 
+//class for Book 
 function Book(title, author, pages){
     this.title = title;
     this.author = author;
@@ -14,6 +15,7 @@ function Book(title, author, pages){
             + hasRead);
     }
 }
+
 //createElements got idea from chris kamp here
 //makes creating elements look a lot nicer and easier to see
 // https://github.com/chris-kamp/bookshelf/blob/ed3478d87fd4e96ad8bce33b661e9b20915dccdd/main.js#L75
@@ -130,24 +132,58 @@ function addNewCard(book){
     });
 }
 
+//toggles the read variable 
 function toggleRead(index){
     myLibrary[index].read = !myLibrary[index].read;
-    //const changeCard = document.querySelector('')
+    const cardToChange = document.querySelector(`.read[data-index='${index}']`)
+    cardToChange.textContent = (myLibrary[index].read === true) ? 'has read' : 'not read yet';
 }
 
+//adds a new book to library and adds it to the screen
 function addBookToLibrary(book){
     addNewCard(book);
     myLibrary.push(book);
 }
 
-function updateBook(book, index){
-
+//gets input from user and adds book to library
+function addNewBook(){
+    
+    const titleValue = document.getElementById('titleInput').value;
+    const authorValue = document.getElementById('authorInput').value;
+    const pagesValue = document.getElementById('pagesInput').value;
+    const newBook = new Book(titleValue, authorValue, pagesValue);
+    addBookToLibrary(newBook);
+    document.getElementById('titleInput').value = '';
+    document.getElementById('authorInput').value = '';
+    document.getElementById('pagesInput').value = '';
 }
 
-function makeCards(library){
-    library.forEach(addNewCard);
+//deletes card and moves rest of array down an index
+function deleteCard(index){
+    const cardTodelete = document.querySelectorAll(`[data-index='${index}']`);
+    cardTodelete.forEach(element =>{
+        element.parentNode.removeChild(element);
+    });
+    myLibrary.splice(index, 1);
+    
+    const libraryCards = document.querySelectorAll('.card');
+    libraryCards.forEach(card => {
+        const cardIndex = card.getAttribute('data-index');
+        if(cardIndex > index){
+            card.setAttribute('data-index', (cardIndex-1));
+            const cardElements = card.querySelectorAll('*');
+            cardElements.forEach(element =>{
+                element.setAttribute('data-index', (cardIndex-1));
+            });
+        }
+    });
 }
+
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295);
 const testBook = new Book('test', 'test', 1);
+
+const addBookButton = document.getElementById('addBook');
+addBookButton.addEventListener('click', addNewBook);
+
 
 addBookToLibrary(theHobbit);
